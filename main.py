baf1ee4bc2d54aa80eec1fb56a30ce7da43f6547
gauravr01 <69200323+gauravr01@users.noreply.github.com>
2023-09-12 09:50:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n - 1)
-
-# Example usage:
 number = 5
 result = factorial(number)
 print(f"The factorial of {number} is {result}")
